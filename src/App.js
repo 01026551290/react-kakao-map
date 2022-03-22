33d7a68f0e08dfb0e2c44651986c82091c98cf66
01026551290 <01026551290@naver.com>
2022-03-22 17:37:29 +0900

    add init for kakaoMap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
 
-function App() {
+const App = () => {
+  
+  useEffect(()=>{
+    const container = document.getElementById('map');
+    const options = {      
+      center: new window.kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
+      level: 3
+    };
+
+    const map = new window.kakao.maps.Map(container, options);
+    const markerPosition  = new window.kakao.maps.LatLng(37.365264512305174, 127.10676860117488); 
+    const marker = new window.kakao.maps.Marker({
+      position: markerPosition
+  });
+  marker.setMap(map);
+
+    }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="map" style={{width:"500px", height:"400px"}}></div>
+    </>
   );
 }
 
